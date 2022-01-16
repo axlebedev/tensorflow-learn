2b4cd9264800c2e9df1bd65af2d0b2eadac3402b
Alex <alexey.broadcast@gmail.com>
2022-01-17 01:18:57 +0300

    wip

--- src/components/TensorFlow/TensorFlow.js
+++ src/components/TensorFlow/TensorFlow.js
@@ -7,29 +7,33 @@ import * as tf from '@tensorflow/tfjs'
 const TensorFlowComponent = () => {
   const model = useMemo(() => {
     // Define a model for linear regression.
-    const result = tf.sequential() // https://js.tensorflow.org/api/latest/#sequential
-    result.add(tf.layers.dense({ units: 1, inputShape: [1] }))
-
-    result.compile({ loss: 'meanSquaredError', optimizer: 'sgd' })
-    return result
+    const modelResult = tf.sequential() // https://js.tensorflow.org/api/latest/#sequential
+    modelResult.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+    modelResult.compile({ loss: 'meanSquaredError', optimizer: 'sgd' })
+    return modelResult
   }, [])
 
   const xs = useMemo(() => {
-    return tf.tensor2d([1, 2, 3, 4], [4, 1])
+    return tf.tensor2d([1, 1, 1, 1], [4, 1])
   }, [])
 
   const ys = useMemo(() => {
-    return tf.tensor2d([1, 3, 5, 7], [4, 1])
+    return tf.tensor2d([1000, 1000, 1000, 1000], [4, 1])
   }, [])
 
-  const onClick = useCallback(() => {
-    // Train the model using the data.
-    model.fit(xs, ys, { epochs: 10 }).then(() => {
+  const onClick = useCallback(
+    async () => {
+      // Train the model using the data.
+      const fitres = await model.fit(xs, ys, { epochs: 10 })
+      console.log('%c11111', 'background:#00FF00', 'fitres=', fitres);
+
       // Use the model to do inference on a data point the model hasn't seen before:
-      const res = model.predict(tf.tensor2d([5], [1, 1]))
-      res.print() // Look at console!
-    })
-  }, [model, xs, ys])
+      const predictres = model.predict(tf.tensor2d([5], [1, 1]))
+      console.log('%c11111', 'background:#00FF00', 'predictres=', predictres);
+      predictres.print() // Look at console!
+    },
+    [model, xs, ys],
+  )
 
   return (
     <div>
